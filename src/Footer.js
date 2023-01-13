8dade888014c0f9d9376343aeb83eec4fe5d196c
jeremy-adamson <jadamson.physics@gmail.com>
2023-01-12 18:40:06 -0800

    Lab2 mostly complete

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 class Footer extends React.Component{
     render(){
         return(
-            <>
+            <footer>
                 <h4>Jeremy Adamson</h4>
-            </>
+            </footer>
         )
     }
 }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,36 @@
-import React from "react"
+import React from "react";
+import Card from 'react-bootstrap/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class HornedBeast extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            favoriteTracker: 0
+        };
+    }
+
+    handleClick = () => {
+        this.setState({
+            favoriteTracker: this.state.favoriteTracker + 1
+        })
+    };
+
     render(){
         return(
-            <>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.img_Url} alt={this.props.title}/>
-                <p>{this.props.description}</p>
-            </>
-        )
+            <div class = "aniCard">
+            <Card style={{ width: '18rem' }}>
+                <Card.Title as="h2">{this.props.title}</Card.Title>
+                <Card.Img src={this.props.img_Url}
+                    alt={this.props.title}
+                    onClick={this.handleClick}/>
+                <Card.Text>{this.props.description}</Card.Text>                
+
+                {this.state.favoriteTracker ? <p>Favorited by: {this.state.favoriteTracker} 🦄</p>:<p>Favorited by none &#x1F922;</p>  }
+            </Card>
+            </div>
+        );
     }
 }
 
